e14bcf0bd3d8641c33edd59768df5c7a29a0d8e8
prateek khened <code.prateekkhened@gmail.com>
2025-08-19 15:47:14 +0530

    simple response to the given prompt

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 def main():
-    print("Hello from aiagent!")
+    import os
+    from dotenv import load_dotenv
+    from google import genai
+    import sys
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    args = sys.argv[1:]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args) 
+
+    res = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+    print(res.text)
+    print("Prompt tokens:", res.usage_metadata.prompt_token_count)
+    print("Response tokens:", res.usage_metadata.candidates_token_count)
+
 
 
 if __name__ == "__main__":
