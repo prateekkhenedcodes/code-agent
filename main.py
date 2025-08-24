34128391a42017430f9d91475ee9f4e85cce0f50
prateek khened <code.prateekkhened@gmail.com>
2025-08-24 15:19:57 +0530

    feedback loop has been set, where agent is given a 20 max iterations to get to the final response

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 from prompts import system_prompt
 from call_function import call_function, available_functions
+from config import MAX_ITERS
 
 
 def main():
@@ -35,8 +36,21 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, verbose)
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
 
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
@@ -46,6 +60,10 @@ def generate_content(client, messages, verbose):
             tools=[available_functions], system_instruction=system_prompt
         ),
     )
+
+    for cand in response.candidates:
+        messages.append(cand.content)
+
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
@@ -64,6 +82,8 @@ def generate_content(client, messages, verbose):
         if verbose:
             print(f"-> {function_call_result.parts[0].function_response.response}")
         function_responses.append(function_call_result.parts[0])
+    
+    messages.append(types.Content(role="user", parts=function_responses))
 
     if not function_responses:
         raise Exception("no function responses generated, exiting.")
